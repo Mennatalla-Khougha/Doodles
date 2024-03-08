ec59872d3552f528e9c28284787617979ce79ef5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-08 18:02:39 +0200

    testing workflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header className="text-3xl font-bold bg-slate-200 space-x-4">
-          <h1 className="bg-gradient-to-r from-blue-600 via-red-400 to-purple-500 inline-block text-transparent bg-clip-text">
+          <h1 className="bg-gradient-to-r from-blue-600 via-red-400 to-green-600 inline-block text-transparent bg-clip-text">
             Doodles
           </h1>
         </header>
